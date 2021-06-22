d3a5deaca1eabf76c0aa5791023f291e83868bba
Rick Rieger <rickrieger36251@gmail.com>
2021-06-22 16:36:55 -0400

    initial commit

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,289 @@
+import React, { Component } from 'react';
+import { isAlpha, isEmail, isAlphanumeric, isStrongPassword } from 'validator';
+import './Signup.css';
+export class Signup extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    firstNameError: '',
+    lastNameError: '',
+    usernameError: '',
+    emailError: '',
+    passwordError: '',
+    confirmPasswordError: '',
+    onConfirmPasswordFocus: false,
+    isButtonDisabled: true,
+  };
+  handleOnChange = (event) => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      () => {
+        if (
+          event.target.name === 'firstName' ||
+          event.target.name === 'lastName'
+        ) {
+          this.handleFirstNameAndLastNameInput(event);
+        }
+        if (event.target.name === 'email') {
+          this.handleEmailInput();
+        }
+        if (event.target.name === 'username') {
+          this.handleUsernameInput();
+        }
+        if (event.target.name === 'password') {
+          this.handlePasswordInput();
+        }
+        if (event.target.name === 'confirmPassword') {
+          if (this.state.password !== this.state.confirmPassword) {
+            this.setState({
+              confirmPasswordError: 'Password does not match!',
+            });
+          } else {
+            this.setState({
+              confirmPasswordError: '',
+            });
+          }
+        }
+      }
+    );
+  };
+
+  handlePasswordInput = () => {
+    if (this.state.onConfirmPasswordFocus) {
+      if (this.state.password !== this.state.confirmPassword) {
+        this.setState({
+          confirmPasswordError: 'Password does not match',
+        });
+      } else {
+        this.setState({
+          confirmPasswordError: '',
+        });
+      }
+    }
+
+    if (this.state.password.length === 0) {
+      this.setState({
+        passwordError: 'Password cannot be empty',
+      });
+    } else {
+      if (isStrongPassword(this.state.password)) {
+        this.setState({
+          passwordError: '',
+        });
+      } else {
+        this.setState({
+          passwordError:
+            'Password must contains 1 uppercase, 1 lowercase, 1 special character, 1 number and minimul of 8 charactors long',
+        });
+      }
+    }
+  };
+  handleEmailInput = () => {
+    if (this.state.email.length === 0) {
+      this.setState({
+        emailError: 'Email cannot be empty',
+      });
+    } else {
+      if (isEmail(this.state.email)) {
+        this.setState({
+          emailError: '',
+        });
+      } else {
+        this.setState({
+          emailError: 'Please, enter a valid email!',
+        });
+      }
+    }
+  };
+  handleFirstNameAndLastNameInput = (event) => {
+    if (this.state[event.target.name].length > 0) {
+      if (isAlpha(this.state[event.target.name])) {
+        this.setState({
+          [`${event.target.name}Error`]: '',
+        });
+      } else {
+        this.setState({
+          [`${event.target.name}Error`]: `${event.target.placeholder} can only have alphabet`,
+        });
+      }
+    } else {
+      this.setState({
+        [`${event.target.name}Error`]: `${event.target.placeholder} cannot be empty`,
+      });
+    }
+  };
+  handleUsernameInput = () => {
+    if (this.state.username.length === 0) {
+      this.setState({
+        usernameError: 'Username cannot be empty',
+      });
+    } else {
+      if (isAlphanumeric(this.state.username)) {
+        this.setState({
+          usernameError: '',
+        });
+      } else {
+        this.setState({
+          usernameError: 'Username can only have alphabet and number',
+        });
+      }
+    }
+  };
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+  handleOnBlur = (event) => {
+    console.log(event.target.name);
+    console.log('handle onBlur Triggered');
+    if (this.state[event.target.name].length === 0) {
+      this.setState({
+        [`${event.target.name}Error`]: `${event.target.placeholder} cannot be empty`,
+      });
+    }
+  };
+  handleConfirmPasswordOnFocus = () => {
+    if (!this.state.onConfirmPasswordFocus) {
+      this.setState({
+        onConfirmPasswordFocus: true,
+      });
+    }
+  };
+  render() {
+    const {
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+      confirmPassword,
+      firstNameError,
+      lastNameError,
+      usernameError,
+      emailError,
+      passwordError,
+      confirmPasswordError,
+    } = this.state;
+    return (
+      <div className="container">
+        <div className="form-text">Sign up</div>
+        <div className="form-div">
+          <form className="form" onSubmit={this.handleOnSubmit}>
+            <div className="form-group-inline">
+              <div className="inline-container">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  value={firstName}
+                  placeholder="First Name"
+                  name="firstName"
+                  onChange={this.handleOnChange}
+                  autoFocus
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {firstNameError && firstNameError}
+                </div>
+              </div>
+              <div className="inline-container">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  value={lastName}
+                  placeholder="Last Name"
+                  name="lastName"
+                  onChange={this.handleOnChange}
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {lastNameError && lastNameError}
+                </div>
+              </div>
+            </div>
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange={this.handleOnChange}
+                  name="email"
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">{emailError && emailError}</div>
+              </div>
+            </div>
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  placeholder="Username"
+                  onChange={this.handleOnChange}
+                  name="username"
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {usernameError && usernameError}
+                </div>
+              </div>
+            </div>
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="text"
+                  id="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={this.handleOnChange}
+                  name="password"
+                  onBlur={this.handleOnBlur}
+                />
+                <div className="errorMessage">
+                  {passwordError && passwordError}
+                </div>
+              </div>
+            </div>
+            <div className="form-group-block">
+              <div className="block-container">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  type="text"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  placeholder="Confirm Password"
+                  onChange={this.handleOnChange}
+                  name="confirmPassword"
+                  onBlur={this.handleOnBlur}
+                  onFocus={this.handleConfirmPasswordOnFocus}
+                />
+                <div className="errorMessage">
+                  {confirmPasswordError && confirmPasswordError}
+                </div>
+              </div>
+            </div>
+            <div className="button-container">
+              <button type="submit" disabled={this.state.isButtonDisabled}>
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+export default Signup;
