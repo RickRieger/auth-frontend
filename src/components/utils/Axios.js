672d00b16a72a535667a01c1c1363fd69b2e5b1c
Rick Rieger <rickrieger36251@gmail.com>
2022-01-05 22:37:01 -0500

    revert the https change to axios in utils

--- src/components/utils/Axios.js
+++ src/components/utils/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const Axios = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "https://localhost:8080/api"
+      ? "http://localhost:8080/api"
       : "/api",
   timeout: 50000,
 });
