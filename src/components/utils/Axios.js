d74ef40c3138aa7e82c71d1f4d3b82fa75b56e89
Rick Rieger <rickrieger36251@gmail.com>
2022-09-15 14:58:25 -0400

    added built version

--- src/components/utils/Axios.js
+++ src/components/utils/Axios.js
@@ -3,7 +3,7 @@ const Axios = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:8080/api'
-      : '/api',
+      : 'https://movie-fun-backend.herokuapp.com/api',
   timeout: 50000,
 });
 export default Axios;
