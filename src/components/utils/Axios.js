165ffc5466112efe83dd9bf3e1cc7ab62ed0ec9a
Rick Rieger <rickrieger36251@gmail.com>
2022-09-15 14:51:39 -0400

    added url for deployed back end on heroku

--- src/components/utils/Axios.js
+++ src/components/utils/Axios.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 const Axios = axios.create({
   baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:8080/api"
-      : "/api",
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8080/api'
+      : 'https://movie-fun-backend.herokuapp.com/api',
   timeout: 50000,
 });
 export default Axios;
